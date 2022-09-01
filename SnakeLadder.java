21ba0262cf0d329851a5d9b6abdd0af85fe820b1
rizry <rizzwan.ansarii@gmail.com>
2022-09-01 23:15:49 +0530

    showed player's starting position

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.java.day4;
+
+public class SnakeLadder {
+
+  public static void main(String[] args) {
+    byte position = 0;
+    System.out.println("The starting position of the player is " + position);
+
+  }
+}
