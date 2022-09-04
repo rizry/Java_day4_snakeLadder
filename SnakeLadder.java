fb30979ddac320045abd0ff06b016dfed5aaebee
rizry <rizzwan.ansarii@gmail.com>
2022-09-04 19:36:46 +0530

    tweaked to accept more than 2 players

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,11 +3,13 @@ package com.java.day4;
 public class SnakeLadder {
   public static void main(String[] ar) throws InterruptedException {
 
-    Player p1 = new Player("John");   //creating 2 objects(players) of Player class
-    Player p2 = new Player("Ken");
-    Player[] players = {p1, p2};    //storing the references of players in a Player type array
+    Player p1 = new Player("Ken");
+    Player p2 = new Player("Mark");
+    Player p3 = new Player("John");
+    Player p4 = new Player("Henry");
+    Player[] players = {p1, p2, p3, p4};
 
-    new GamePlay(players);    //passing the players to the constructor of GamePlay class to begin the gameplay.
+    new GamePlay(players);
   }
 }
 
@@ -27,7 +29,7 @@ class GamePlay {
   }
 
   void startGame() throws InterruptedException {
-    System.out.println("Snake and Ladder game is starting now.");
+    System.out.println("game is starting now.");
 
     do {
       byte dieRollNum = (byte) ((Math.random() * 10) % 6 + 1);
@@ -39,7 +41,7 @@ class GamePlay {
       switch (playOption) {
 
         case OPTION_NO_PLAY:
-          System.out.println(currentPlayer.name + " not playing. staying at " + currentPlayer.position + "\n");
+          System.out.println(currentPlayer.name + "'s not playing. staying at " + currentPlayer.position + "\n");
           switchPlayer();
           break;
 
@@ -49,12 +51,14 @@ class GamePlay {
             currentPlayer.position = 0;
             if (currentPlayer.flag) {
               currentPlayer.flag = false;
-              System.out.println("stepped on snake and " + currentPlayer.name + "'s right back where " + currentPlayer.name + " started. but " + currentPlayer.name + " wont give up\n");
+              System.out.println("stepped on snake and " + currentPlayer.name + "'s right back where " + currentPlayer.name
+                + " started. but " + currentPlayer.name + " wont give up\n");
               switchPlayer();
               continue;
             }
           }
-          System.out.println("Aww. " + currentPlayer.name + " stepped on snake. going back to " + currentPlayer.position + " :( \n");
+          System.out
+          .println("Aww. " + currentPlayer.name + " stepped on snake. going back to " + currentPlayer.position + " :( \n");
           switchPlayer();
           break;
 
@@ -63,33 +67,34 @@ class GamePlay {
           if (currentPlayer.position + dieRollNum <= 100) {
             currentPlayer.position += dieRollNum;
 
-            System.out.println("Yay! Ladder. off " + currentPlayer.name + " goes to position " + currentPlayer.position + " :) \n");
+            System.out
+            .println("Yay! Ladder. off " + currentPlayer.name + " goes to position " + currentPlayer.position + " :) \n");
             continue;
           }
           System.out.println("welp. guess " + currentPlayer.name + " will just stay at " + currentPlayer.position + " until "
             + currentPlayer.name + " gets " + (100 - currentPlayer.position) + "\n");
           break;
-      }
+        }
       Thread.sleep(10);
 
     } while (currentPlayer.position < 100);
 
-    System.out.println("whew! it took " + dieRollTimes + " die rolls in total and "+ currentPlayer.name+" finally won");
+    System.out.println("whew! it took " + dieRollTimes + " die rolls in total and " + currentPlayer.name + " finally won");
   }
 
   void switchPlayer() {
-    System.out.println(
-      players[0].name + "'s position " + players[0].position + ". " + players[1].name + "'s position " + players[1].position);
-
-    if (currentPlayer == players[0]) {
-      currentPlayer = players[1];
-    } else {
-      currentPlayer = players[0];
+    for (Player p : players) System.out.print(p.name + "'s at position " + p.position + ". ");
+       
+    for (byte i = 0; i < players.length; i++) {
+      if (currentPlayer == players[i]) {
+        if (i == players.length - 1) i = -1;
+        currentPlayer = players[i + 1];
+        break;
+      }
     }
 
-    System.out.println(currentPlayer.name + "'s turn now \n");
+    System.out.println("\n" + currentPlayer.name + "'s turn now \n");
   }
-
 }
 
 class Player {
@@ -97,7 +102,7 @@ class Player {
   boolean flag = false;
   byte position = 0;
 
-  Player(String name) {   //accepting player's name in a parameterized constructor.
+  Player(String name) {
     this.name = name;
   }
 }
